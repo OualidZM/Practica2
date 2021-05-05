54ae7722546b0bf32aa3c0f2e2020b77c3d46fe9
OualidZM <ozaaj@cifpfbmoll.eu>
2021-05-05 15:41:17 +0200

    docs: :construction: App.java
    I added the menu that will be displayed, when the User chooses to run the program

--- practica2/src/main/java/edu/pingpong/practica2/App.java
+++ practica2/src/main/java/edu/pingpong/practica2/App.java
@@ -8,6 +8,8 @@ public class App
 public static final Scanner startProgram = new Scanner(System.in);
 public static final Scanner menu = new Scanner(System.in);
 public static final Logica  logica = new Logica();
+public static  boolean salir = false;
+
 
 
     public static void main( String[] args )
@@ -28,7 +30,43 @@ public static final Logica  logica = new Logica();
                         + "\n" +"\t" +
 
                 "[99]" + " " + "Quit program" );
+
+        do{
+
+            int run = startProgram.nextInt();
+
+
+                if(startProgram.hasNextLine()){
+
+                System.out.println("\t" + "@||||||@ Practica 2 @||||||@"
+                + "\n" + "\n" +"\t" + 
+                "Elige la Opcion que quiere realizar:" 
+                + "\n" + "\n" +"\t" +
+                "[1]" +  " " +  "Ask 5 numbers and display them with the same order " 
+                + "\n" +"\t" +
+                "[2]" +  " " +  "Ask 5 numbers and display them in the reverse order " 
+                + "\n" +"\t" +
+                "[3]" +  " " +  "Ask 5 numbers and then make avarage of positives and negatives, and finally count all \"0\""
+                + "\n" +"\t" +
+                "[4]" +  " " +  "Ask for text and return the length of it without spaces"
+                + "\n" +"\t" +
+                "[5]" +  " " +  "ask for text and return it inverted"
+                + "\n" +"\t" +
+                "[6]" +  " " +  "Ask for two texts and return it without spaces"
+                + "\n" +"\t" +
+                "[7]" +  " " +  "Ask for two texts and concatenate them"
+                + "\n" +"\t" +
+                "[8]" +  " " +  "Ask for a text and a vowel, then replace all the vowels with the one that was asked"
+                + "\n" +"\t" +
+                "[9]" +  " " +  "Ask for a text and show it's ascii code"
+                + "\n" +  "\n" + 
+                "[99]" +  " " +  "-Quit program"
+                );
+                }
+                
+            }while(!salir);
         }
+
 
         catch (Exception e) {
             System.out.println("SOMETHING WENTTTTT WRONG");
