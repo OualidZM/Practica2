75590677c48d6413a23fdb9a81303f9d6b216307
OualidZM <ozaaj@cifpfbmoll.eu>
2021-05-05 16:48:00 +0200

    docs: :construction: Logica.java
    The method1  is completed

--- practica2/src/main/java/edu/pingpong/practica2/Logica.java
+++ practica2/src/main/java/edu/pingpong/practica2/Logica.java
@@ -1,14 +1,30 @@
 package edu.pingpong.practica2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Logica {
     
 
     public static final App app = new App();
+    public static List listt;
+    public static final Scanner input = new Scanner(System.in);
 
 
-public void  method1(){
+    Logica(){
+        this.listt = new ArrayList<>();
+    }
 
-}
+
+    public static void method1() {
+        
+        while (listt.size() < 5) {   
+            System.out.println("Please enter a positive number:");  
+            listt.add(input.nextInt()); 
+        }
+            System.out.println("Result:"  + listt);
+    }
 
 public void  method2(){
     
@@ -59,50 +75,50 @@ public void  method9(){
 
     
 
-    public int Switch() {
-        
-        int menuu =  app.menu.nextInt();
-        switch (menuu) {
-            case 1:
-                method1();
-                break;
-
-            case 2:
-            method2();
-            break;
-
-            case 3:
-            method3();
-            break;
-
-            case 4:
-            method4();
-            break;
-
-            case 5:
-            method5();
-            break;
-
-            case 6:
-            method6();
-            break;
-
-            case 7:
-            method7();
-            break;
-
-            case 8:
-            method8();
-            break;
-            
-            case 9:
-            method9();
-            break;
+public int Switch() {
     
-            default:
-            System.out.println("wrong!");
-                break;
+    int menuu =  app.menu.nextInt();
+    switch (menuu) {
+        case 1:
+            method1();
+            break;
+
+        case 2:
+        method2();
+        break;
+
+        case 3:
+        method3();
+        break;
+
+        case 4:
+        method4();
+        break;
+
+        case 5:
+        method5();
+        break;
+
+        case 6:
+        method6();
+        break;
+
+        case 7:
+        method7();
+        break;
+
+        case 8:
+        method8();
+        break;
+        
+        case 9:
+        method9();
+        break;
+
+        default:
+        System.out.println("wrong!");
+            break;
         }
     }
-    
+
 }
